5fd585b3cbc88ff7cd1e84b83a1b03a292fdb24e
asenetcky <asenetcky@gmail.com>
2024-10-29 14:34:38 -0400

    add tests for make_header()

---
+++ tests/testthat/test-make_header.R
@@ -0,0 +1,30 @@
+test_that("require all elements", {
+  expect_error(make_header())
+})
+
+
+test_that("makes the same xml", {
+  expect_equal(
+    make_header(
+      mcn = "test",
+      jurisdiction_code = "test",
+      content_group_id = "test",
+      submitter_email = "test",
+      submitter_name = "test",
+      submitter_title = "test",
+      state_fips_code = "test"
+    ),
+    xml2::read_xml(
+      paste0(
+        "<Header><MCN>test</MCN><JurisdictionCode>test",
+        "</JurisdictionCode><ContentGroupIdentifier>test",
+        "</ContentGroupIdentifier><SubmitterInformation>",
+        "<SubmitterEmailAddress>test</SubmitterEmailAddress>",
+        "<SubmitterName>test</SubmitterName>",
+        "<SubmitterTitle>test</SubmitterTitle>",
+        "</SubmitterInformation><StateFIPSCode>test",
+        "</StateFIPSCode></Header>"
+      )
+    )
+  )
+})
