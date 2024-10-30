7e402f6fdbc6ebf3938e06f708cf553293ecb85c
asenetcky <asenetcky@gmail.com>
2024-10-30 15:36:59 -0400

    add make_document doco, tests and export

--- R/make_document.R
+++ R/make_document.R
@@ -1,3 +1,49 @@
+#' Create an XML document
+#'
+#' @param data Pre-wrangled dataframe
+#' @param content_group_id Code that identifies the content
+#' @param mcn Metadata Control Number provided by epht
+#' @param jurisdiction_code Two-letter state abbreviation
+#' @param state_fips_code FIPS code of the state
+#' @param submitter_email Email of person submitting data to epht
+#' @param submitter_name First and last name of person submitting data
+#' @param submitter_title Title of person submitting data
+#'
+#' @return XML document
+#' @export
+#'
+#' @examples
+#' data <-
+#' mtcars |>
+#'   dplyr::rename(
+#'     month = mpg,
+#'     agegroup = cyl,
+#'     county = disp,
+#'     ethnicity = hp,
+#'     health_outcome_id = drat,
+#'     monthly_count = wt,
+#'     race = qsec,
+#'     sex = vs,
+#'     year = am
+#'   ) |>
+#'   dplyr::select(-c(gear, carb))
+#' content_group_id <- "AS-HOSP"
+#' mcn <- "1234-1234-1234-1234-1234"
+#' jurisdiction_code <- "two_letter_code"
+#' state_fips_code <- "1234"
+#' submitter_email <- "submitter@email.com"
+#' submitter_name <- "Submitter Name"
+#' submitter_title <- "Submitter Title"
+#'doc <- make_document(
+#'  data,
+#'  content_group_id,
+#'  mcn,
+#'  jurisdiction_code,
+#'  state_fips_code,
+#'  submitter_email,
+#'  submitter_name,
+#'  submitter_title
+#')
 make_document <-
   function(data,
            content_group_id,
@@ -30,4 +76,5 @@ make_document <-
   xml2::xml_add_child(xml_document, root_element)
 
 
+  xml_document
 }
