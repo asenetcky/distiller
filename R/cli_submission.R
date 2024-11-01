d6e1fe47e9f63fccb16c53003147f677bbae9238
asenetcky <asenetcky@gmail.com>
2024-11-01 15:17:00 -0400

    move create_exit_status to cli_submission

--- R/cli_submission.R
+++ R/cli_submission.R
@@ -37,3 +37,79 @@
 ### has_character
 ### has length
 ### has_format
+#
+# f <- function() {
+#   size <- 0L
+#   cli::cli_progress_step(
+#     "Downloading data.",
+#     msg_failed = "Downloaded {prettyunits::pretty_bytes(size)}.",
+#     spinner = TRUE
+#   )
+#   for (i in 1:100) {
+#     Sys.sleep(3/100)
+#     size <- size + 8192
+#     cli::cli_progress_update()
+#   }
+# }
+# f()
+#
+#
+message_cli <- function(status_list) {
+
+  if (status_list$code == 0) {
+    cli::cli_alert_success(status_list$message)
+  }
+
+  if (status_list$code == 1) {
+    cli::cli_alert_danger(status_list$message)
+  }
+
+  if (status_list$code == 2) {
+    cli::cli_alert_warning(status_list$message)
+  }
+
+}
+#
+# pkgs <- c("foo", "bar", "foobar")
+# cli::cli_alert_success("Downloaded {length(pkgs)} packages.")
+
+
+#TODO: for target variable name, use both the object name and the value in glue
+create_exit_status <- function(
+    target_variable_name,
+    warn_variables = NULL,
+    danger_variables = NULL) {
+
+  exit_status <-
+    dplyr::lst(
+      code = 0,
+      message = glue::glue("Success")
+    )
+
+  if (!is.null(warn_variables)) {
+
+    warn <- any(!warn_variables)
+
+    if (warn) {
+      exit_status <-
+        dplyr::lst(
+          code = 2,
+          message = glue::glue("Warning: {target_variable_name} may not have correct format")
+        )
+    }
+  }
+
+  if (!is.null(danger_variables)) {
+
+    danger <- any(!danger_variables)
+
+    if (danger) {
+      exit_status <-
+        dplyr::lst(
+          code = 1,
+          message = glue::glue("Danger: {target_variable_name} is not allowable value")
+        )
+    }
+  }
+  exit_status
+}
