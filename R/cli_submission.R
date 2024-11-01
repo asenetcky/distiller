5e3cc92f8fadc301030389bb83555193b2703aab
asenetcky <asenetcky@gmail.com>
2024-11-01 15:27:05 -0400

    add check name to success message

--- R/cli_submission.R
+++ R/cli_submission.R
@@ -1,59 +1,4 @@
 
-# TODO something like this:
-# see: https://cli.r-lib.org/reference/cli_progress_step.html
-# f <- function() {
-# msg <- ""
-# cli_progress_step("Downloading data{msg}", spinner = TRUE)
-# for (i in 1:100) {
-#   Sys.sleep(2/100)
-#   msg <- glue::glue(", got file {i}/100")
-#   cli_progress_update()
-# }
-# cli_progress_step("Importing data")
-# Sys.sleep(1)
-# cli_progress_step("Cleaning data")
-# Sys.sleep(2)
-# cli_progress_step("Fitting model", spinner = TRUE)
-# for (i in 1:100) { Sys.sleep(3/100); cli_progress_update() }
-# }
-# f()
-
-
-#success
-#warn
-#danger
-#info
-
-
-#create exit status 0 for success
-#exit status 1 for danger 2 for warn
-
-
-## content_group_id
-### has_character
-### has_allowable_id
-
-## mcn
-### has_character
-### has length
-### has_format
-#
-# f <- function() {
-#   size <- 0L
-#   cli::cli_progress_step(
-#     "Downloading data.",
-#     msg_failed = "Downloaded {prettyunits::pretty_bytes(size)}.",
-#     spinner = TRUE
-#   )
-#   for (i in 1:100) {
-#     Sys.sleep(3/100)
-#     size <- size + 8192
-#     cli::cli_progress_update()
-#   }
-# }
-# f()
-#
-#
 message_cli <- function(status_list) {
 
   if (status_list$code == 0) {
@@ -69,9 +14,6 @@ message_cli <- function(status_list) {
   }
 
 }
-#
-# pkgs <- c("foo", "bar", "foobar")
-# cli::cli_alert_success("Downloaded {length(pkgs)} packages.")
 
 
 #TODO: for target variable name, use both the object name and the value in glue
@@ -83,7 +25,7 @@ create_exit_status <- function(
   exit_status <-
     dplyr::lst(
       code = 0,
-      message = glue::glue("Success")
+      message = glue::glue("Success: {target_variable_name}")
     )
 
   if (!is.null(warn_variables)) {
