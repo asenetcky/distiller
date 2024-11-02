81d48aa78785a4d43061cf27ef019a158205d977
asenetcky <asenetcky@gmail.com>
2024-11-02 06:39:07 -0400

    add wip code for clearer cli messaging

--- R/cli_submission.R
+++ R/cli_submission.R
@@ -1,5 +1,7 @@
 
 message_cli <- function(status_list) {
+  checkmate::assert_list(status_list)
+  checkmate::assert_subset(names(status_list), c("code", "message"))
 
   if (status_list$code == 0) {
     cli::cli_alert_success(status_list$message)
@@ -12,7 +14,6 @@ message_cli <- function(status_list) {
   if (status_list$code == 2) {
     cli::cli_alert_warning(status_list$message)
   }
-
 }
 
 
@@ -28,22 +29,36 @@ create_exit_status <- function(
       message = glue::glue("Success: {target_variable_name}")
     )
 
+
   if (!is.null(warn_variables)) {
 
     warn <- any(!warn_variables)
 
+
     if (warn) {
+     names(warn_variables) <- deparse(substitute(warn_variables))
+     warn_variables <- warn_variables[warn_variables == FALSE]
+     troublemakers <- paste(names(warn_variables), collapse = ", ")
+
       exit_status <-
         dplyr::lst(
           code = 2,
-          message = glue::glue("Warning: {target_variable_name} may not have correct format")
+          message = glue::glue(
+            "Warning: {target_variable_name} may not have correct format
+              Troublemakers: {troublemakers}"
+          )
         )
     }
   }
 
   if (!is.null(danger_variables)) {
-
+    danger_vars <- set_names(danger_variables, names(danger_variables))
     danger <- any(!danger_variables)
+
+
+
+    #maybe something like this:
+    #{tar var} has restricted values in: {danger_variables}
 
     if (danger) {
       exit_status <-
@@ -54,4 +69,15 @@ create_exit_status <- function(
     }
   }
   exit_status
+}
+
+list_troublemakers <- function(vars) {
+  var_string <- ""
+
+  for (var in names(vars)) {
+    var_name <- names(var)
+    glue::glue("{var_string}{var_name}, ")
+  }
+
+  var_string
 }
