e433eb6486c6db4b517c9560017f8ac6d9614224
asenetcky <asenetcky@gmail.com>
2024-11-04 12:00:06 -0500

    add tests for cli_submission

--- tests/testthat/test-cli_submission.R
+++ tests/testthat/test-cli_submission.R
@@ -80,3 +80,23 @@ test_that("create_exit_status creates right code and message", {
     list(code = 2, message = "Warning: test_variable may not have correct format")
   )
 })
+
+test_that("message_cli runs without error",{
+  status_list_success <- list(code = 0, message = "Success: test_variable")
+  status_list_danger <- list(code = 1, message = "Danger: test_variable does not have allowable value/s")
+  status_list_warn <- list(code = 2, message = "Warning: test_variable may not have correct format")
+  expect_no_error(message_cli(list(code = 3, message = "unknown")))
+  expect_no_error(message_cli(status_list_success))
+  expect_no_error(message_cli(status_list_danger))
+  expect_no_error(message_cli(status_list_warn))
+})
+
+test_that("list_troublemakers returns right string", {
+  mixed <- c(TRUE, FALSE, TRUE) |> purrr::set_names(c("true", "false", "true"))
+  all_true <- c(TRUE, TRUE, TRUE) |> purrr::set_names(c("true", "true", "true"))
+  all_false <- c(FALSE, FALSE, FALSE) |> purrr::set_names(c("false", "false", "false"))
+
+  expect_equal(list_troublemakers(mixed), "false")
+  expect_equal(list_troublemakers(all_true), "")
+  expect_equal(list_troublemakers(all_false), "false, false, false")
+})
