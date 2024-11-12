7fbe2a69a0a68483f4797a098a212615cc8523f3
asenetcky <asenetcky@gmail.com>
2024-11-12 14:48:56 -0500

    update doco on check_submission

--- R/check_submission.R
+++ R/check_submission.R
@@ -1,3 +1,49 @@
+#' Check the validity of a submission
+#'
+#' @param data dataframe to be converted to XML
+#' @param content_group_id  Code that identifies the content
+#' @param mcn Metadata Control Number provided by EPHT
+#' @param jurisdiction_code Two-letter state abbreviation
+#' @param state_fips_code FIPS code of the state
+#' @param submitter_email Email of person submitting data to EPHT
+#' @param submitter_name First and last name of person submitting data
+#' @param submitter_title Title of person submitting data
+#'
+#' @return cli list in terminal of submission check results
+#' @export
+#'
+#' @examples
+#' data <-
+#' mtcars |>
+#'   dplyr::rename(
+#'     month = mpg,
+#'     agegroup = cyl,
+#'     county = disp,
+#'     ethnicity = hp,
+#'     health_outcome_id = drat,
+#'     monthly_count = wt,
+#'     race = qsec,
+#'     sex = vs,
+#'     year = am
+#'   ) |>
+#'   dplyr::select(-c(gear, carb))
+#' content_group_id <- "AS-HOSP"
+#' mcn <- "1234-1234-1234-1234-1234"
+#' jurisdiction_code <- "two_letter_code"
+#' state_fips_code <- "1234"
+#' submitter_email <- "submitter@email.com"
+#' submitter_name <- "Submitter Name"
+#' submitter_title <- "Submitter Title"
+#' check_submission(
+#'  data,
+#'  content_group_id,
+#'  mcn,
+#'  jurisdiction_code,
+#'  state_fips_code,
+#'  submitter_email,
+#'  submitter_name,
+#'  submitter_title
+#')
 check_submission <-
   function(data,
            content_group_id,
