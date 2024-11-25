9615fd9d8fb40f599c2faf5e39986feeabf1ed21
asenetcky <asenetcky@gmail.com>
2024-11-24 20:16:47 -0500

    add placeholder helpers

---
+++ R/check-drinking_water_content.R
@@ -0,0 +1,63 @@
+# xml names for
+# pws
+# "PWSIDNumber",
+# "Year",
+# "AnalyteCode",
+# "DateSampled",
+# "AggregationType",
+# "NumSamplingLocations",
+# "SummaryTimePeriod",
+# "NumSamples",
+# "NumNonDetects",
+# "ConcentrationUnits",
+# "Concentration"
+
+# wql
+# "PWSIDNumber",
+# "YearAssociatedTo",
+# "YearPulled",
+# "PWSName",
+# "PrincipalCountyServedFIPS",
+# "PrincipalCityFeatureID",
+# "TotalConnections",
+# "SystemPopulation",
+# "PrimarySourceCode",
+# "Latitude",
+# "Longitude",
+# "LocationDerivationCode"
+
+placeholder_example <- function(data) {
+  county <- NULL
+
+  has_length <-
+    FALSE |>
+    purrr::set_names("length")
+
+  has_character <-
+    checkmate::check_character(data$county) |>
+    is.logical() |>
+    purrr::set_names("class")
+
+  if (has_character) {
+    has_unknown <-
+      any("U" %in% data$county)
+
+    if (has_unknown) {
+      data <-
+        data |>
+        dplyr::filter(county != "U")
+    }
+
+    has_length <-
+      all(
+        stringr::str_length(data$county) == 5
+      ) |>
+      purrr::set_names("length")
+  }
+
+  create_exit_status(
+    "county",
+    warn_variables = has_character,
+    danger_variables = has_length
+  )
+}
