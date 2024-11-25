ee02b65aa30b87b6db1b5e6b9685b7e5d3f266a6
asenetcky <asenetcky@gmail.com>
2024-11-25 10:51:59 -0500

    add check_pwisid_number

--- R/check-drinking_water_content.R
+++ R/check-drinking_water_content.R
@@ -61,3 +61,40 @@ placeholder_example <- function(data) {
     danger_variables = has_length
   )
 }
+
+check_pwisid_number <- function(data) {
+  pwsid_number <- NULL
+
+  has_length <-
+    FALSE |>
+    purrr::set_names("length")
+
+  has_format <-
+    FALSE |>
+    purrr::set_names("format")
+
+  has_character <-
+    checkmate::check_character(data$pwsid_number) |>
+    is.logical() |>
+    purrr::set_names("class")
+
+  if (has_character) {
+    has_length <-
+      all(
+        stringr::str_length(data$pwsidnumber) == 9
+      ) |>
+      purrr::set_names("length")
+
+    has_format <-
+      all(
+        stringr::str_detect(data$pwsid_number, "^[A-Z]{2}[0-9]{7}$")
+      ) |>
+      purrr::set_names("format")
+  }
+
+  create_exit_status(
+    "pwsid_number",
+    warn_variables = has_character,
+    danger_variables = c(has_length, has_format)
+  )
+}
