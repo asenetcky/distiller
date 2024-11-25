a70b1880b17b4d4bf445212ce479194e55661bd3
asenetcky <asenetcky@gmail.com>
2024-11-25 12:33:52 -0500

    add check-checlist for wql

---
+++ R/check-wql_content.R
@@ -0,0 +1,20 @@
+#check state_fips_code
+# The following looks misspelled in the doco - better check the xml to see
+# that it's actually the same as the pws inventory
+# check pwsid_number
+# check year
+# check analyte code
+# check concentration_units
+# check concentration
+# check date_sampled
+# check aggreation_type
+# check num_sampling_locations
+# check summary_time_period
+# check num_samples
+# check num_non_Detects
+
+# things I cant see but ar ein doco
+# check sample_point_id
+# check check detection_limit
+# check non_detect_flag (probably an agg of nondetects?)
+#
